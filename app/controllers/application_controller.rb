1337a2f9c50da8a20789fc30d6d349f7610ccf35
yoshizooo <y-i1996@yahoo.ne.jp>
2023-05-21 15:00:48 +0900

    新規ユーザー登録機能の実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up,
+                                      keys: [:nickname, :children_name, :guodian_name, :class_name_id])
+  end
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,17 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]{6,}+\z/i.freeze
+
+  validates :password,
+            format: { with: VALID_PASSWORD_REGEX, message: ' is invalid. Include both letters and numbers', allow_blank: true }
+  
+  validates :class_name_id, numericality: { other_than: 1, message: "can't be blank" }
+  validates :children_name, presence: true
+  validates :guodian_name, presence: true
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :class_name
+  
 end
