5322cc3b7dd92df01cc95fc7978163e374cba3f0
yoshizooo <y-i1996@yahoo.ne.jp>
2023-05-23 19:56:28 +0900

    adminの追加

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Admin.find_or_create_by(id: 1) do |admin|
+  admin.admin_name ="admin"
+  admin.email = "example@example.com"
+  admin.password = "example"
+  admin.admin_number ="1234"
+  admin.user_id ="1"
+ end
