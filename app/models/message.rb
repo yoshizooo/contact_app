5a3a66a8fdcb99b7e896689340ed18b94914896a
yoshizooo <y-i1996@yahoo.ne.jp>
2023-06-14 22:00:25 +0900

    画像投稿機能２

--- app/models/message.rb
+++ app/models/message.rb
@@ -2,8 +2,13 @@ class Message < ApplicationRecord
   belongs_to :room
   has_one :user_message, dependent: :destroy
   has_one :admin_message, dependent: :destroy
-  has_one_attached :image
+  has_one_attached :image, ependent: :destroy
 
-  validates :content, presence: true
+  validates :content, presence: true, unless: :was_attached?
+
+  def was_attached?
+    self.image.attached?
+  end
+
 
 end
